5100f7efc32db55c168d9d0a8d660b8129620c98
Witold Mormul <Witold Mormul>
2017-02-13 16:34:16 +0100

    Initial commit

---
+++ src/com/jhf/Portal.java
@@ -0,0 +1,36 @@
+package com.jhf;
+/**
+ * Created by Witold on 13.02.2017.
+ */
+
+import java.util.*;
+
+public class Portal {
+    private ArrayList<String> polaPolozenia;
+    private String nazwa;
+
+    public void setPolaPolozenia(ArrayList<String> ppol) {
+        polaPolozenia = ppol;
+    }
+
+    public void setNazwa(String nzwPortalu){
+        nazwa = nzwPortalu;
+    }
+
+    public String sprawdz(String ruch) {
+        String wynik = "pudlo";
+        int indeks = polaPolozenia.indexOf(ruch);
+        if (indeks >= 0) {
+            polaPolozenia.remove(indeks);
+
+            if(polaPolozenia.isEmpty()) {
+                wynik = "zatopiony";
+                System.out.println("Auć! Zatopiłeś portal" + nazwa + " :(");
+            }
+            else {
+                wynik = "trafiony";
+            } // end if
+        } // end if
+        return wynik;
+    } // end sprawdz
+} // end com.jhf.Portal
---
+++ src/com/jhf/PortalGra.java
@@ -0,0 +1,75 @@
+package com.jhf;
+
+import java.util.*;
+import java.lang.String;
+
+
+public class PortalGra {
+    private PomocnikGry pomocnik = new PomocnikGry();
+    private ArrayList<Portal> listaPortali = new ArrayList<Portal>();
+    private int iloscRuchow = 0;
+
+    private void przygotujGre() {
+        //tworzymy portale i okreslamy ich polozenie
+        Portal pierwszy = new Portal();
+        pierwszy.setNazwa("pornhub.com");
+        Portal drugi = new Portal();
+        drugi.setNazwa("youporn.com");
+        Portal trzeci = new Portal();
+        trzeci.setNazwa("redtube.com");
+        listaPortali.add(pierwszy);
+        listaPortali.add(drugi);
+        listaPortali.add(trzeci);
+
+        System.out.println("Twoim celem jest zatopienie trzech portali.");
+        System.out.println("pornhub.com, youporn.com, redtube.com");
+        System.out.println("Postaraj się je zatopić w jak najmniejszej ilości ruchów.");
+
+        for (Portal rozmieszczanyPortal : listaPortali) {
+            ArrayList<String> nowePolozenie = pomocnik.rozmiescPortal(3);
+            rozmieszczanyPortal.setPolaPolozenia(nowePolozenie);
+        } // end for
+    } // end przygotujGre
+
+    private void rozpocznijGre() {
+        while (!listaPortali.isEmpty()) {
+            String ruchGracza = pomocnik.pobierzDaneWejsciowe("Podaj pole:");
+            sprawdzRuchGracza(ruchGracza);
+        } // end while
+    } // end rozpocznijGre
+
+    private void sprawdzRuchGracza(String ruch) {
+        iloscRuchow++;
+        String wynik = "pudlo";
+
+        for (Portal portalDosprawdzenia : listaPortali) {
+            wynik = portalDosprawdzenia.sprawdz(ruch);
+            if (wynik.equals("trafiony")) {
+                break;
+            }
+            if (wynik.equals("zatopiony")) {
+                listaPortali.remove(portalDosprawdzenia);
+                break;
+            }
+        } // end for
+        System.out.println(wynik);
+    } // sprawdzRuchGracza
+
+    private void zakonczGre() {
+        System.out.println("Wszystkie portale zostały zatopione!");
+        if (iloscRuchow <= 18) {
+            System.out.println("Wykonales jedynie " + iloscRuchow + " ruchów.");
+            System.out.println("Zwycięstwo!");
+        }
+        else {
+            System.out.println("Strasznie sie grzebales! Zajelo Ci to az " + iloscRuchow + "ruchów!");
+            System.out.println("Przegrałeś!");
+        }
+    } // end zakonczGre
+
+    public static void main(String[] args) {
+        PortalGra gra = new PortalGra();
+        gra.przygotujGre();
+        gra.rozpocznijGre();
+    }
+}
